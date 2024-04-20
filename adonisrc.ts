016490393517e9d9e0e89863db84133726129aff
Raffael Di Pietro <maxou4170@gmail.com>
2024-04-20 11:11:26 +0200

    feat(cors): configure cors

--- adonisrc.ts
+++ adonisrc.ts
@@ -28,7 +28,8 @@ export default defineConfig({
       file: () => import('@adonisjs/core/providers/repl_provider'),
       environment: ['repl', 'test'],
     },
-    () => import('@adonisjs/core/providers/vinejs_provider')
+    () => import('@adonisjs/core/providers/vinejs_provider'),
+    () => import('@adonisjs/cors/cors_provider')
   ],
 
   /*
